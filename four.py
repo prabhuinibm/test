a6533782bb0e2f5c277746c5d04d9fa125067499
Prabhu <prabhuinibm@gmail.com>
2023-01-20 19:10:27 -0500

    update 4.py with mul

--- four.py
+++ four.py
@@ -1,9 +1,15 @@
-element = [10,20,31,56,78]
-for i in element:
+element_list = [10,20,31,56,78]
+for i in element_list:
     print(i)
 #if odd is double and even is square
     if (i %2 ==0):
         print(i*i)
     else:
         print(i*2)
+# div 3 should mul by 9 , if div by 4 mul by 16
+for i in element_list:
+    if i % 3 == 0:
+        print(f"{i} {i*9}, CSP your answer")
+    elif i % 4 == 0:
+        print(f"{i} {i*16}, There you go CSP")
     
